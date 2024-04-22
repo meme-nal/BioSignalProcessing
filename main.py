708702a4e315087ccf6624ff0ad54cbe1d417c6b
DeltaC <georgiy.fedorov.lyceum@yandex.ru>
2024-04-22 20:36:11 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mne
 import matplotlib.pyplot as plt
 
 # getting the raw data
-raw_data = mne.io.read_raw_edf('record.edf', preload=True)
+raw_data = mne.io.read_raw_edf('./data/record.edf', preload=True)
 
 # dropping bad channels
 bad_channels = raw_data.ch_names[-5:-1]
@@ -10,14 +10,14 @@ bad_channels.append("SAO2-1")
 preprocessed_data = raw_data.drop_channels(bad_channels)
 
 # finding events
-#events = mne.events_from_annotations(preprocessed_data)
-#print(events)
+events = mne.events_from_annotations(preprocessed_data)
+print(events)
 
-delta_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=0.5, h_freq=4)
-theta_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=4, h_freq=8)
-alpha_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=8, h_freq=13)
-beta_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=13, h_freq=30)
-gamma_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=30, h_freq=170)
+#delta_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=0.5, h_freq=4)
+#theta_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=4, h_freq=8)
+#alpha_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=8, h_freq=13)
+#beta_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=13, h_freq=30)
+#gamma_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=30, h_freq=170)
 
 # plotting eeg diagram
 #gamma_freq_preprocessed_data.plot()
@@ -26,6 +26,6 @@ gamma_freq_preprocessed_data = preprocessed_data.copy().filter(l_freq=30, h_freq
 #theta_freq_preprocessed_data.plot()
 #delta_freq_preprocessed_data.plot()
 #preprocessed_data.plot()
-plt.show()
+#plt.show()
 
 
